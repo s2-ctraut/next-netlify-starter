b3b83f97b2fad597ccf6da60290eccca82cc6e67
Christoph Trautwein <christoph.trautwein@sinnerschrader.com>
2021-07-13 16:58:16 +0200

    a async

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -5,7 +5,7 @@ import { Handler } from "@netlify/functions";
 
 let counter = 0;
 
-const handler: Handler = (_event, _context) => {
+const handler: Handler = async (_event, _context) => {
   // const name = event.queryStringParameters.name || "World";
   const name = "World";
 
