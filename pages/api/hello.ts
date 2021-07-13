2efe7a8ce203753badea55704bfe83541b0dcdf7
Christoph Trautwein <christoph.trautwein@sinnerschrader.com>
2021-07-13 15:46:00 +0200

    add counter

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,8 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { Handler } from "@netlify/functions";
 
+let counter = 0;
+
 const handler: Handler = async (_event, _context) => {
   console.log("Greetings from hello.ts");
+  console.log(`Cpunt = #{counter}`);
+  counter++;
   return {
     statusCode: 200,
     body: JSON.stringify({ name: "Jane Doe" }),
