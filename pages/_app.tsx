11fd66ed88a5e83b69313f59950ef9fe3a0b633c
Christoph Trautwein <christoph.trautwein@sinnerschrader.com>
2021-07-21 16:05:41 +0200

    fix 1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/global.css'
 import { AppProps } from 'next/app'
 
+// global CSS imports
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
