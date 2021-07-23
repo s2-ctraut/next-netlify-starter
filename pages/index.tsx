e586bb46563c8ab9b273f3c541f90d915e401c6e
Christoph Trautwein <christoph.trautwein@sinnerschrader.com>
2021-07-23 16:55:42 +0200

    ux

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,9 +33,9 @@ export default function Home({
         <Head>
           <title>{siteTitle}</title>
         </Head>
-        <App />
-        <hr />
         <CtExperiment />
+        <hr />
+        <App />
       </Layout>
     </QueryClientProvider>
   )
