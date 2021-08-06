7e6ade7f986499e771d50c630711284904e70007
Christoph Trautwein <christoph.trautwein@sinnerschrader.com>
2021-08-06 15:57:19 +0200

    f counter

--- components/CtExperiment.tsx
+++ components/CtExperiment.tsx
@@ -1,4 +1,5 @@
-import { useQuery } from 'react-query';
+import { QueryClient, QueryClientProvider, QueryKey, useQuery } from 'react-query';
+import { ReactQueryDevtools } from "react-query/devtools";
 
 // Components
 import LinearProgress from '@material-ui/core/LinearProgress';
@@ -6,38 +7,88 @@ import Button from '@material-ui/core/Button';
 
 // Styles
 import { Wrapper } from '../styles/App.styles';
+import React, { useEffect } from 'react';
 
 // Types
-export type AddingType = {
+export type AddResultType = {
   counter: number;
   sum: string;
 };
 
-const getSum = async (): Promise<AddingType> =>
-  await (await fetch('/.netlify/functions/add?arg1=342&arg2=102')).json();
+export type AddArgsType = {
+  arg1: number;
+  arg2: number;
+};
+
+const queryClient = new QueryClient();
 
 const CtExperiment = () => {
-  const { data, isLoading, error } = useQuery<AddingType>(
-    'addition',
+/*
+fetch('https://www.learnwithjason.dev/graphql', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    query: `
+        query GetLearnWithJasonEpisodes($now: DateTime!) {
+          allEpisode(limit: 10, sort: {date: ASC}, where: {date: {gte: $now}}) {
+            date
+            title
+            guest {
+              name
+              twitter
+            }
+            description
+          }
+        }
+      `,
+    variables: {
+      now: new Date().toISOString(),
+    },
+  }),
+})
+  .then((res) => res.json())
+  .then((result) => console.log(result));
+*/
+
+
+  const [arg2, setArg2] = React.useState(0);
+
+  const getSum = async (): Promise<AddResultType> =>
+    await (await fetch(`/.netlify/functions/add?arg1=342&arg2=${arg2}`,{ })).json();
+
+  const { data, isLoading, error } = useQuery<AddResultType,AddArgsType>(
+    ['arg2', arg2],
     getSum
   );
   console.log(data);
+
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${arg2} times`;
+  });
 
   if (isLoading) return <LinearProgress />;
   if (error) return <div>Something went wrong ...</div>;
 
   const handleCoolStuff = () => {
     console.log('Handling cool stuff...');
+    setArg2(arg2+2);
+    console.log(`arg2 =  ${arg2}`);
   }
 
   return (
     <Wrapper>
+      <QueryClientProvider client={queryClient}>
       <div>
         <Button onClick={() => handleCoolStuff()}>Do cool stuff</Button>
 
         <h3>Sum: {data?.sum}</h3>
         <h3>Counter: {data?.counter}</h3>
       </div>
+      <ReactQueryDevtools initialIsOpen />
+      </QueryClientProvider>
     </Wrapper>
   );
 };
