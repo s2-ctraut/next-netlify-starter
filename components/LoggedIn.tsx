51a68c7974bbfbf63be799fadcb440a97479ffb6
Christoph Trautwein <christoph.trautwein@sinnerschrader.com>
2021-09-17 16:30:09 +0200

    f ui

--- components/LoggedIn.tsx
+++ components/LoggedIn.tsx
@@ -8,31 +8,31 @@ import SocialButton from './SocialButton';
 const clientConfig = { google: { app_id: '261377444261-5ovk2irequohduu8eddis19l5ofvfl53.apps.googleusercontent.com' }};
 
 const LoggedIn: React.FC = () => {
-  const [gProfile, setGProfile] = useState<RemoteProfileProps>();
+  const [rProfile, setRemoteProfile] = useState<RemoteProfileProps>();
   const handleGoogleSocialLogin = (user: any) => {
     console.log(user);
     console.log('Profile:');
     console.log(user.profile);
-    setGProfile(user.profile);
+    setRemoteProfile(user.profile);
   };
   
   const handleSocialLoginFailure = (err: any) => {
     console.error(err);
   };
   return(
-      <div>
-        <SocialButton
-          provider="google"
-          appId={clientConfig.google.app_id}
-          onLoginSuccess={handleGoogleSocialLogin}
-          onLoginFailure={handleSocialLoginFailure}
-        >
-          Login with google
-        </SocialButton>
-        {
-          gProfile &&
-          <RemoteProfile {...gProfile}/>
-        }
+    <div>
+      {
+        rProfile? 
+        <RemoteProfile {...rProfile}/>
+        : <SocialButton
+            provider="google"
+            appId={clientConfig.google.app_id}
+            onLoginSuccess={handleGoogleSocialLogin}
+            onLoginFailure={handleSocialLoginFailure}
+          >
+            Login with google
+          </SocialButton>
+      }
     </div>
     );
   };
